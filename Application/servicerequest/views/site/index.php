d6dd5cb2031433ff6cd1e0cb92f393f3b4f78567
Kenneth <kennethabuel@hotmail.com>
2017-08-07 10:40:48 +0800

    Fixed Forms

---
+++ Application/servicerequest/views/site/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+
+        <p class="lead">SM Hotels - Service Request Management System.
+        <br>
+   
+
+    </div>
+
+
+
+</div>
