e08c34473cbe4ac8e234e28aebd3897bdd89f9a3
Kenneth <kennethabuel@hotmail.com>
2017-08-14 21:48:03 +0800

    Updated @employee/_search.php

--- Application/servicerequest/views/employee/_search.php
+++ Application/servicerequest/views/employee/_search.php
@@ -21,6 +21,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'servEmployee_fname') ?>
 
+    <?= $form->field($model, 'servEmployee_status') ?>
+
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
