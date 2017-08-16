9b32eae15870f57672a62bcdd80b82e7ca535bbf
Kenneth <kennethabuel@hotmail.com>
2017-08-16 20:06:41 +0800

    Added Access Control @servicerequest/controllers/ServiceRequestController.php

--- Application/servicerequest/controllers/ServiceRequestController.php
+++ Application/servicerequest/controllers/ServiceRequestController.php
@@ -8,6 +8,7 @@ use app\models\ServiceRequestSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * ServiceRequestController implements the CRUD actions for Servicerequest model.
@@ -20,6 +21,16 @@ class ServiceRequestController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['create', 'update'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@']
+                    ],
+                ]
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
