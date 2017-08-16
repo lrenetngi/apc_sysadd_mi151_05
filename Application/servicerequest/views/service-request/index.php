4291dc5f431e19742942a4afa94fb76c69bbed4e
Kenneth <kennethabuel@hotmail.com>
2017-08-16 16:22:24 +0800

    Adding Bootstrap alert @service-request/index.php

--- Application/servicerequest/views/service-request/index.php
+++ Application/servicerequest/views/service-request/index.php
@@ -21,6 +21,13 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'rowOptions' => function ($model)
+        {
+            if($model->request_status = 'Done')
+            {
+                return['class' => 'success'];
+            }
+        },
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
