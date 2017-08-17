af9abddba0bde4881f5cf01805de46cba9135ff1
Kenneth <kennethabuel@hotmail.com>
2017-08-18 05:29:19 +0800

    Added DateTime Picker Widget @shift/_form.php

--- Application/servicerequest/views/shift/_form.php
+++ Application/servicerequest/views/shift/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\datetime\DateTimePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Shift */
@@ -12,13 +13,24 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'sched_start')->textInput() ?>
+    <?= $form->field($model, 'sched_start')->widget(
+    						 DateTimePicker::className(), [
+    						 	'options' => ['placeholder' => 'Time in'],
+    						 	'pluginOptions' => [
+    						 	'autoclose' => true,
+    						 	]
+    						 ]
+    ) ?>
 
-    <?= $form->field($model, 'sched_end')->textInput() ?>
+    <?= $form->field($model, 'sched_end')->widget(
+    						 DateTimePicker::className(), [
+    						 'options' => ['placeholder' => 'Time Out'],
+    						 'pluginOptions' => [
+    						 'autoclose' => true,
+    						 ]
+    						]
+    ) ?>
 
-    <?= $form->field($model, 'department_id')->textInput() ?>
-
-    <?= $form->field($model, 'supervisor')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
