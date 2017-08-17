de87ddf710903d6afbae19c802b510b494a78146
Kenneth <kennethabuel@hotmail.com>
2017-08-18 00:28:32 +0800

    Generated Crud for Employees

---
+++ Application/servicerequest/views/employee/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Employee */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="employee-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'emp_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'position')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'department_id')->textInput() ?>
+
+    <?= $form->field($model, 'position_id')->textInput() ?>
+
+    <?= $form->field($model, 'shift_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
