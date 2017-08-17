9dba1ad44f15660673551bca6da22e4cdc9db488
Kenneth <kennethabuel@hotmail.com>
2017-08-18 02:50:02 +0800

    Generated CRUD for Tickets

---
+++ Application/servicerequest/views/ticket/_form.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Ticket */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ticket-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'request_title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'time_start')->textInput() ?>
+
+    <?= $form->field($model, 'time_end')->textInput() ?>
+
+    <?= $form->field($model, 'time_alloted')->textInput() ?>
+
+    <?= $form->field($model, 'escalation_level')->textInput() ?>
+
+    <?= $form->field($model, 'desc')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'check_in_id')->textInput() ?>
+
+    <?= $form->field($model, 'employee_respond_id')->textInput() ?>
+
+    <?= $form->field($model, 'department_id')->textInput() ?>
+
+    <?= $form->field($model, 'category_id')->textInput() ?>
+
+    <?= $form->field($model, 'employee_create_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ Application/servicerequest/views/ticket/_search.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\TicketSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ticket-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'request_title') ?>
+
+    <?= $form->field($model, 'status') ?>
+
+    <?= $form->field($model, 'time_start') ?>
+
+    <?= $form->field($model, 'time_end') ?>
+
+    <?php // echo $form->field($model, 'time_alloted') ?>
+
+    <?php // echo $form->field($model, 'escalation_level') ?>
+
+    <?php // echo $form->field($model, 'desc') ?>
+
+    <?php // echo $form->field($model, 'check_in_id') ?>
+
+    <?php // echo $form->field($model, 'employee_respond_id') ?>
+
+    <?php // echo $form->field($model, 'department_id') ?>
+
+    <?php // echo $form->field($model, 'category_id') ?>
+
+    <?php // echo $form->field($model, 'employee_create_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
