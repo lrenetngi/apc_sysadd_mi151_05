1e2afc11c3eafe54f7ea02a41a92918b90c440a8
Kenneth <kennethabuel@hotmail.com>
2017-08-17 23:25:07 +0800

    Changed Database

--- Application/servicerequest/config/db.php
+++ Application/servicerequest/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=servicemanagement',
+    'dsn' => 'mysql:host=localhost;dbname=servicerequest',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
