b04ad0a69b80d86139fdae8d4bc7ea3593190b50
Lurenne <vinobryant7@gmail.com>
2017-08-22 13:04:32 +0800

    added @Application/servicerequest/controllers/ShiftController.php

--- Application/servicerequest/controllers/ShiftController.php
+++ Application/servicerequest/controllers/ShiftController.php
@@ -4,7 +4,7 @@ namespace app\controllers;
 
 use Yii;
 use app\models\Shift;
-use app\models\ShiftSearch;
+use app\models\ShiftModel;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -35,7 +35,7 @@ class ShiftController extends Controller
      */
     public function actionIndex()
     {
-        $searchModel = new ShiftSearch();
+        $searchModel = new ShiftModel();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
--- Application/servicerequest/views/shift/_form.php
+++ Application/servicerequest/views/shift/_form.php
@@ -2,9 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use kartik\datetime\DateTimePicker;
-use app\models\Department;
-use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Shift */
@@ -15,32 +12,13 @@ use yii\helpers\ArrayHelper;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'sched_start')->widget(
-    						 DateTimePicker::className(), [
-    						 	'options' => ['placeholder' => 'Time in'],
-    						 	'pluginOptions' => [
-    						 	'autoclose' => true,
-    						 	]
-    						 ]
-    ) ?>
+    <?= $form->field($model, 'sched_start')->textInput() ?>
 
-    <?= $form->field($model, 'sched_end')->widget(
-    						 DateTimePicker::className(), [
-    						 'options' => ['placeholder' => 'Time Out'],
-    						 'pluginOptions' => [
-    						 'autoclose' => true,
-    						 ]
-    						]
-    ) ?>
+    <?= $form->field($model, 'sched_end')->textInput() ?>
 
-    <?= $form->field($model, 'department_id')->dropDownlist(
-                                ArrayHelper::map(Department::find()->all(), 'dept_name', 'dept_name'),
-                                [
-                                    'prompt' => 'Select Department',
-                                    'style' => 'width:250px'
-                                ]
-    ); ?>
+    <?= $form->field($model, 'department_id')->textInput() ?>
 
+    <?= $form->field($model, 'supervisor')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
