e2535f6fc9f1b62abb614a805c9d67c1bc481665
Lurenne <vinobryant7@gmail.com>
2017-08-22 13:03:26 +0800

    added @Application/servicerequest/controllers/RoomController.php

--- Application/servicerequest/controllers/RoomController.php
+++ Application/servicerequest/controllers/RoomController.php
@@ -4,7 +4,7 @@ namespace app\controllers;
 
 use Yii;
 use app\models\Room;
-use app\models\RoomSearch;
+use app\models\RoomModel;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -35,7 +35,7 @@ class RoomController extends Controller
      */
     public function actionIndex()
     {
-        $searchModel = new RoomSearch();
+        $searchModel = new RoomModel();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
